93d986333ba117085e3d64d9a3ba91e8ea5e0f19
Hiroki Chiyoda <contact@arafipro.com>
2024-08-10 14:39:06 +0900

    tutorialディレクトリのパスを変更

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,7 +2,7 @@ import type * as Preset from "@docusaurus/preset-classic";
 import type { Config } from "@docusaurus/types";
 import { themes as prismThemes } from "prism-react-renderer";
 
-const SITE_TITLE = "Docusaurus Test Site"; 
+const SITE_TITLE = "Docusaurus Test Site";
 const GITHUB_URL = "https://github.com/afafipro/";
 const GITHUB_EDIT_URL =
   "https://github.com/arafipro/docusaurus-website-test/blob/main/";
@@ -31,7 +31,7 @@ const config: Config = {
   // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "ja",
-    locales: ["ja", "en"],
+    locales: ["ja"],
   },
 
   presets: [
@@ -89,7 +89,7 @@ const config: Config = {
           items: [
             {
               label: "Tutorial",
-              to: "/docs/intro",
+              to: "/docs/tutorial/intro",
             },
           ],
         },
